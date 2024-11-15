711230cef9b291727ff1af2e8291cf158af6f210
uygar <uygarb00@gmail.com>
2024-11-15 16:21:44 +0300

    Initial Commit 4

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def webhook():
 
 
 
-if __name__ == '__main__':
+if __name__ == '__app__':
     port = int(os.environ.get("PORT", 5000))  # Railway otomatik olarak PORT ayarlar
     app.run(host="0.0.0.0", port=port, debug=True)
